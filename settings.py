fb3093267e8379e832ec35ad976deba43dcf297a
Hua Zhu <hua.zhu@ericsson.com>
2017-09-25 20:18:18 +0800

    Initial commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+
+    def __init__(self):
+
+        self.screen_width = 900
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+
+        # ship sittings
+        self.ship_speed_factor = 1.5
